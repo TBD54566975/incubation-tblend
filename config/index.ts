676097f5e5d4a38fb9c8c54113b7acfeb1036dba
Ian Patton <53611782+ianpatton@users.noreply.github.com>
2023-12-15 11:03:36 -0500

    build

--- config/index.ts
+++ config/index.ts
@@ -1,9 +1,7 @@
-import pkg from "../package.json";
-
 let scheme = "https";
 const externalHostname = process.env.EXTERNAL_HOSTNAME || "localhost";
 const externalPort = parseInt(`${process.env.EXTERNAL_PORT}`) || 3000;
-const serviceName = process.env.SERVICE_NAME || pkg.name;
+const serviceName = process.env.SERVICE_NAME || "verifiable-credential-issuer";
 
 let swaggerHost = externalHostname;
 if (externalPort !== 443) {
@@ -18,7 +16,6 @@ const config = {
         host: swaggerHost,
         info: {
             title: `${serviceName} Documentation`,
-            version: pkg.version,
         },
         schemes: [scheme],
         jsonPath: "/api/swagger.json",
